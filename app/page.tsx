cc550f64446e84abae4eff43ca6522fbba00a350
Velyn Cash <dev@velyn.cash>
2025-12-30 22:53:02 +0700

    Create page.tsx

---
+++ app/page.tsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <main style={{ padding: 24, fontFamily: "system-ui" }}>
+      <h1>Velyn.Cash</h1>
+      <p>velyn-web :: frontend scaffold</p>
+    </main>
+  );
+}
